7d35d28c09b70e5d6ae44c1fff5542b78b30b381
Wojciech Mlynarczyk <wojciech.mlynarczyk+github@gmail.com>
2019-02-22 17:20:45 +0100

    Fixed location of linux_64 jni files

--- ros2-java-maven/src/main/scripts/copy_jni_files_to_target_classes.sh
+++ ros2-java-maven/src/main/scripts/copy_jni_files_to_target_classes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir -p target/classes
-find ~/ros2_java_ws/build_isolated/ -name '*.so*' -exec cp {} target/classes  \;
+mkdir -p target/classes/natives/linux_64/
+find ~/ros2_java_ws/build_isolated/ -name '*.so*' -exec cp {} target/classes/natives/linux_64/  \;
